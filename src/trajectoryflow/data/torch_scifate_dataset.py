84c6d7a628c9fb7953e68fc193874f262a1444f7
Chaitanya Sai Patibandla <p.chaitanya0412@gmail.com>
2026-06-21 21:51:51 +0200

    Implement dataset length and item retrieval methods

--- src/trajectoryflow/data/torch_scifate_dataset.py
+++ src/trajectoryflow/data/torch_scifate_dataset.py
@@ -7,8 +7,17 @@ class TorchScifateDataset(Dataset):
     def __init__(self, scifate_dataset):
         self.scifate_dataset = scifate_dataset
 
+        self.expression_matrix = (
+            scifate_dataset.expression_matrix.to_np()
+        )
+
     def __len__(self):
-        return 0
+        return self.expression_matrix.shape[0]
 
     def __getitem__(self, idx):
-        return None
+        x = torch.tensor(
+            self.expression_matrix[idx],
+            dtype=torch.float32
+        )
+
+        return x
