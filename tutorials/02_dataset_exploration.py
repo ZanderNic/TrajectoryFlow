5ba010fb9ba09b46935e1131edc828c539ba6935
Chaitanya Sai Patibandla <p.chaitanya0412@gmail.com>
2026-06-21 22:03:33 +0200

    Add gene info exploration to dataset tutorial

--- tutorials/02_dataset_exploration.py
+++ tutorials/02_dataset_exploration.py
@@ -19,3 +19,9 @@ print(scifate_data.cell_info.columns)
 
 print("\nFirst 5 rows:")
 print(scifate_data.cell_info.head())
+
+print("\nGene info columns:")
+print(scifate_data.gene_info.columns)
+
+print("\nFirst 5 genes:")
+print(scifate_data.gene_info.head())
